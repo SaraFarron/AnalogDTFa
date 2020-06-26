61bccaa46442f339e99e3d4a56f5ef8517cb1a83
SerahFarron <jiperscreepers@gmail.com>
2020-06-26 23:18:10 +0700

    now im dead lol

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def upvote():
-    post_rating+=1
+    articles.post_rating+=1
 
 def downvote():
-    post_rating-=1
+    articles.post_rating-=1
 
-def add_comment():
-    comments_number+=1
+#def add_comment():
+    #comments_number+=1
